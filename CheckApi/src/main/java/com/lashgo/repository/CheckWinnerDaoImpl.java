2083e89ddd24ec02433f71f4b34c2f6d8fc6d7fa
petriyov.e <eugene.petriyov@gmail.com>
2014-10-26 03:23:54 +0600

    fixed check winner error

--- CheckApi/src/main/java/com/lashgo/repository/CheckWinnerDaoImpl.java
+++ CheckApi/src/main/java/com/lashgo/repository/CheckWinnerDaoImpl.java
@@ -21,8 +21,8 @@ public class CheckWinnerDaoImpl implements CheckWinnersDao {
                         "  (SELECT p.check_id,p.user_id FROM photos p " +
                         "    INNER JOIN " +
                         "(SELECT al1.photo_id as photo_id, MAX(al1.votes_count) as votes_count FROM" +
-                        "(SELECT uvv.photo_id, pp.make_date as photo_date, COUNT(uvv.id) AS votes_count FROM user_votes uvv " +
-                        "INNER JOIN photos pp ON (pp.id = uvv.photo_id AND pp.check_id = ?) GROUP BY photo_id) al1 GROUP BY al1.photo_id order by votes_count, photo_date desc limit 1) al2" +
+                        "(SELECT uvv.photo_id, COUNT(uvv.id) AS votes_count FROM user_votes uvv " +
+                        "INNER JOIN photos pp ON (pp.id = uvv.photo_id AND pp.check_id = ?) GROUP BY photo_id) al1 GROUP BY al1.photo_id order by votes_count desc limit 1) al2" +
                         " on (al2.photo_id = p.id)" +
                         "    GROUP BY p.check_id,p.user_id)", check
         );
