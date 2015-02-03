fabaae8395b8e7483be1c604360b45e2d337fdb1
petriyov.e <eugene.petriyov@gmail.com>
2015-02-03 23:05:55 +0600

    докоммичено

--- CheckApi/src/main/java/com/lashgo/CheckConstants.java
+++ CheckApi/src/main/java/com/lashgo/CheckConstants.java
@@ -1,4 +1,4 @@
-package com.lashgo;
+﻿package com.lashgo;
 
 /**
  * Created by Eugene on 19.03.14.
@@ -12,10 +12,10 @@ public final class CheckConstants {
     public static final String FACEBOOK_DATE_FORMAT = "dd/MM/yyyy";
     public static final String GCM_PROJECT_API = "AIzaSyATgI7pv6bmHfuxa8nCy2O0IKUy36x_I8c";
     public static final String DATE_FORMAT = "dd.MM.yyyy HH:mm:ss Z";
-    public static final String PHOTOS_FOLDER = "C:/DATA/Archive/LashGo/photos";
+//    public static final String PHOTOS_FOLDER = "C:/DATA/Archive/LashGo/photos";
     public static final int DURATION = 6;
     public static final int VOTE_DURATTON = 3;
-//    public static final String PHOTOS_FOLDER = "/home/lashgo/photos/";
+    public static final String PHOTOS_FOLDER = "/home/lashgo/photos/";
 
     private CheckConstants() {
 
